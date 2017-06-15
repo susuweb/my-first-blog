5be0cd97d614890da2ae22a9be739546c6f698b3
susuweb <swb888168@qq.com>
2017-06-15 23:27:36 +0800

    Create admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import sys;
+import importlib
+import sys
 
-reload(sys);
+importlib.reload(sys)
 sys.setdefaultencoding("utf8")
 
 from django.contrib import admin
